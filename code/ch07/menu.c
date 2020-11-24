fd9f40bd363e87476dcbc350b41a5010c2e16c07
0x-kor <70549608+0x-kor@users.noreply.github.com>
2020-11-24 20:37:59 +0900

    Update menu.c

--- code/ch07/menu.c
+++ code/ch07/menu.c
@@ -5,13 +5,13 @@ int main(void) {
 	int input;
 
 	do {
-		printf("\t [1] ÇÑ½Ä\n");
-		printf("\t [2] ¾ç½Ä\n");
-		printf("\t [3] ºĞ½Ä\n");
-		printf("\t [4] ±âÅ¸\n");
-		printf("¸Ş´º ¹øÈ£ ¼±ÅÃ ÈÄ [Enter] :");
+		printf("\t [1] í•œì‹\n");
+		printf("\t [2] ì–‘ì‹\n");
+		printf("\t [3] ë¶„ì‹\n");
+		printf("\t [4] ê¸°íƒ€\n");
+		printf("ë©”ë‰´ ë²ˆí˜¸ ì„ íƒ í›„ [Enter] :");
 		scanf("%d", &input);
-		printf("¼±ÅÃ ¸Ş´º %d\n, input");
+		printf("ì„ íƒ ë©”ë‰´ %d\n", input);
 
 		if (input <= 4 && input >= 1)
 			break;
