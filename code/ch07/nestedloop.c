c859f752d38c13764e7af045e4aefa77d1b841e8
0x-kor <70549608+0x-kor@users.noreply.github.com>
2020-11-24 20:42:36 +0900

    Update nestedloop.c

--- code/ch07/nestedloop.c
+++ code/ch07/nestedloop.c
@@ -4,7 +4,7 @@ int main(void) {
 	int m, n;
 
 	for (m = 1; m <= 5; m++) {
-		printf("m = %-2d\n, m");
+		printf("m = %-2d\n", m);
 		for (n = 1; n <= 7; n++)
 			printf("n = %-3d", n);
 		puts("");
